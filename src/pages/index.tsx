a9d78e7e8549e5eb58c14868e0e103125c72e142
Jorge Gustavo <jorgegustavo500@gmail.com>
2023-03-14 12:04:32 -0300

    versão 1

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+  return (
+    <>
+     <Head>
+       <title>Sujeito Pizza</title>
+     </Head>
+     <div>
+       <h1>Jorge</h1>
+     </div>
+    </>
+  )
+}
