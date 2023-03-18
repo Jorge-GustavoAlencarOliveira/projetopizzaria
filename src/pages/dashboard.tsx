956d8cc4ca3a21646f3e0dd91f48f8fcd8a1d360
Jorge-GustavoAlencarOliveira <jorgegustavo500@gmail.com>
2023-03-18 11:48:48 -0300

    1.2

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,16 +2,55 @@ import { canSSRAuth } from '@/Utils/CanSSRAuth'
 import React from 'react'
 import Head from 'next/head'
 import Header from '@/Components/Header'
-const Dashboard = () => {
+import styles from '../../styles/dashboard.module.scss'
+import { FiRefreshCcw } from 'react-icons/fi'
+import { setupAPICliente } from '@/Services/api'
+
+type OrdersProps = {
+  name?: string | null,
+  id: string,
+  table: string | number,
+  draft: boolean,
+  status: boolean,
+}
+
+interface listProps{
+  ordersList: OrdersProps[];
+}
+
+const Dashboard = ({ordersList}:listProps) => {
+  const [orders, setOrders] = React.useState(ordersList || []);
+
+  function handleModal(order_id : string){
+    alert(order_id)
+  }
+
   return (
     <>
       <Head>
         <title>Painel - Sujeito Pizzaria</title>
       </Head>
       <Header />
-      <div>
-        Dashboard
-      </div>
+      <main className={styles.container}>
+        <div className={styles.header}>
+          <h1>Últimos pedidos</h1>
+          <button>
+            <FiRefreshCcw size={25} color='3fffa3'/>
+          </button>
+        </div>
+        <article className={styles.listOrders}>
+          {orders.map(item => {
+            return(
+              <section key={item.id} className={styles.orderItem}>
+                <button onClick={() => handleModal(item.id)}>
+                  <div className={styles.tag}></div>
+                  <span>Mesa {item.table}</span>
+                </button>
+              </section>
+            )
+          })}
+        </article>
+      </main>
     </>
   )
 }
@@ -19,7 +58,14 @@ const Dashboard = () => {
 export default Dashboard
 
 export const getServerSideProps = canSSRAuth( async(ctx) =>{
+  const api = setupAPICliente(ctx);
+  const response = await api.get('/orders/list')
+  const orders = response.data
+  console.log(orders)
+  
   return {
-    props:{}
+    props:{
+      ordersList: orders
+    }
   }
 })
